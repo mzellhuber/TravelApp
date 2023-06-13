cf41aea16e5b6892b7fd73aced2113a9fd75dc53
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-06-13 10:25:34 -0600

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Melissa Zellhuber on 13/06/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TravelAppApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
