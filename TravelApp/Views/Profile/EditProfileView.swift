0a5aa53f477bb23157b6eba00015683ec53f4717
Melissa Zellhuber <melissa.zellhuber@gmail.com>
2023-06-21 07:42:03 -0600

    Added button and created view

---
+++ TravelApp/Views/Profile/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  TravelApp
+//
+//  Created by Melissa Zellhuber on 21/06/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
--- TravelApp/Views/Profile/ProfileView.swift
+++ TravelApp/Views/Profile/ProfileView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import os
 
 struct ProfileView: View {
     @State private var userName: String = "John Doe"
@@ -7,7 +8,12 @@ struct ProfileView: View {
     @State private var bannerImage: Image = Image(systemName: "photo")
     @State private var userLocation: String = "🇺🇸 San Francisco"
     @State private var showSettingsView: Bool = false
-
+    
+    private let logger = Logger(
+        subsystem: "Profile",
+        category: String(describing: ProfileView.self)
+    )
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -18,38 +24,54 @@ struct ProfileView: View {
                         .frame(height: 200)
                         .clipped()
                         .edgesIgnoringSafeArea(.top)
-
-                    userImage
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
-                        .shadow(radius: 10)
-                        .offset(x: 20, y: -70)
-                        .padding(.bottom, -50)
+                    
+                    HStack {
+                        userImage
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                            .shadow(radius: 10)
+                            .padding()
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // Handle edit button action
+                            logger.debug("Edit button tapped!")
+                        }) {
+                            Image(systemName: "pencil")
+                                .font(.system(size: 20))
+                                .foregroundColor(.white)
+                                .padding(8)
+                                .background(Color.purple)
+                                .clipShape(Circle())
+                        }
+                    }
+                    .padding(.trailing, 20)
                 }
-
+                
                 Text(userName)
                     .font(.largeTitle)
                     .fontWeight(.medium)
                     .padding(.top, 50)
-
+                
                 Text(userEmail)
                     .font(.title2)
                     .padding(.top, 10)
-
+                
                 Text(userLocation)
                     .font(.title2)
                     .padding(.top, 10)
-
+                
                 Spacer()
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarItems(trailing:
                 Button(action: {
                     showSettingsView = true
-                    print("Gear icon tapped!")
+                    logger.debug("Gear icon tapped!")
                 }) {
                     Image(systemName: "gearshape")
                         .font(.system(size: 20))
@@ -65,7 +87,6 @@ struct ProfileView: View {
         }
     }
 }
-
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
