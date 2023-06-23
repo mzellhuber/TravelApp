c86af3951be773079d18f7e08e24389c4eef17e3
Mariam Babutsidze <mariambabutsidze@gmail.com>
2023-06-23 18:35:19 +0400

    adds views on explore view

---
+++ TravelApp/Views/ExploreView/ExploreView.swift
@@ -0,0 +1,54 @@
+//
+//  ExploreView.swift
+//  TravelApp
+//
+//  Created by Mariam Babutsidze on 13.06.23.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    @State private var searchText: String = ""
+    @State private var searchIsActive = false
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text("Where do you want \nto go?")
+                            .font(.title)
+                            .bold()
+                            .lineLimit(2, reservesSpace: true)
+                        Spacer()
+                    }
+                    TripsView()
+                    CategoriesView()
+                }
+                .padding(.leading, 20)
+                .padding(.trailing, 20)
+                .toolbar {
+                    ToolbarItem {
+                        Button(action: {}, label: {
+                            Image(systemName: "person.circle")
+                        })
+                        .tint(.black)
+                    }
+                    ToolbarItem (placement: .navigation) {
+                        HStack {
+                            Text("Hi, Mariam,")
+                                .font(.subheadline)
+                                .bold()
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            .background(.lightGrey)
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
